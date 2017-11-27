7e7e35539480071d6ea8e5fac7ba1827c4c9eb85
Elatha <bernat.90@gmail.com>
2017-11-27 20:57:42 +0100

    Working adding string to text

--- app/src/main/java/com/topoinventari/sampleapp/MainActivity.java
+++ app/src/main/java/com/topoinventari/sampleapp/MainActivity.java
@@ -1,33 +1,62 @@
 package com.topoinventari.sampleapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQ_ENTER_TEXT = 1;
+
+
+    private List<String> lines;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        lines = new ArrayList<>();
+
         setContentView(R.layout.activity_main);
 
-        //Layout is inflated
+        //Layout is inflated, sempre despres de setContentView perque sino no trobara les coses de la pantalla
         initView();
     }
 
     private void initView() {
 
-        TextView text = findViewById(R.id.text);//Troba elements de la vista a traves del id
-        Button button = findViewById(R.id.button);
+        Button button = findViewById(R.id.button); //Troba elements de la vista a traves del id
 
         button.setOnClickListener((v) -> {
-            text.setText("Clicked!!!");
+            //open new activity
+            Intent intent = new Intent(this, EnterTextActivity.class); //On estem i on anem...
+            startActivityForResult(intent,REQ_ENTER_TEXT);
+
+
         });
 
     }
 
+    //This is called when coming from an activity started with startActivityForResult
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == REQ_ENTER_TEXT) {
+
+            String text = data.getStringExtra(EnterTextActivity.EXTRA_TEXT);
+            lines.add(text);
+
+            TextView textView = findViewById(R.id.text);
+            textView.setText(lines.toString());
+
+        }
+
+    }
 }
